6c6e51c6fcfff64fa46e93e600656a9b9d44981d
ASUS <faresaction123>
2021-11-13 04:49:30 +0100

    Model change commit

--- programs/models.py
+++ programs/models.py
@@ -5,4 +5,4 @@ class Task(models.Model):
     title = models.CharField(max_length=255)
     completed = models.BooleanField(default=False, blank=True, null=False )
     def __str__(self):
-        return self.title
+        return self.title + str(int(self.completed))
